563ae0458cb20ddf9f8688e15911fef2ba5d0319
Giovanni <gio.a.98@gmail.com>
2021-12-16 13:32:04 +0100

    init + axios/bootstrap

---
+++ src/components/macro/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: "Main",
+    data() {
+        return {
+            albums: null,
+        }
+    },
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            this.albums = response.data.response;
+        });
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
