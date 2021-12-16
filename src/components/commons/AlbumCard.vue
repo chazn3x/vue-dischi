30a5d3a649eb0a3048c5b9439ca44ad03c17eecd
Giovanni <gio.a.98@gmail.com>
2021-12-16 16:58:23 +0100

    fine bonus

--- src/components/commons/AlbumCard.vue
+++ src/components/commons/AlbumCard.vue
@@ -3,7 +3,7 @@
         <div class="img">
             <img :src="info.poster" :alt="info.title + ' cover'">
         </div>
-        <h2>{{info.title}}</h2>
+        <h3>{{info.title}}</h3>
         <p>{{info.author}}</p>
         <p>{{info.year}}</p>
     </div>
@@ -23,17 +23,23 @@ export default {
     background: rgb(45,58,70);
     padding: 15px;
     text-align: center;
-    color: white;
+    border-radius: 3px;
     .img {
         width: 100%;
         aspect-ratio: 1;
+        overflow: hidden;
         margin-bottom: 10px;
         img {
             width: 100%;
         }
     }
-    h2 {
+    h3 {
         margin-bottom: 15px;
+        color: white;
+        text-transform: uppercase;
+    }
+    p {
+        color: rgba(255,255,255,0.4);
     }
 }
 </style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,6 +1,7 @@
 <template>
     <main>
-        <AlbumsList :albums="albums"/>
+        <AlbumsList v-if="albums.length == 10" :albums="albums"/>
+        <div v-else class="loader"></div>
     </main>
 </template>
 
@@ -14,7 +15,7 @@ export default {
     },
     data() {
         return {
-            albums: null,
+            albums: [],
         }
     },
     mounted() {
@@ -27,5 +28,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.loader {
+    width: 60px;
+    height: 60px;
+    margin: 150px auto;
+    border-radius: 50%;
+    border: 4px solid rgb(45,58,70);
+    border-top: 4px solid rgb(28,215,96);
+    // border: 4px solid rgb(28,215,96);
+    animation: spin 1s linear infinite;
+    @keyframes spin {
+        0% {transform: rotate(0deg);}
+        100% {transform: rotate(360deg);}
+    }
+}
 </style>
